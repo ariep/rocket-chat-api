88bf3d1e017ab8d81bf6d19a7a47e42ffe933b31
Arie Peterson <ariepeterson@radicallyopensecurity.com>
2017-04-25 16:13:45 +0200

    Look up users by username

--- lib/RocketChat/Api/User.php
+++ lib/RocketChat/Api/User.php
@@ -10,6 +10,7 @@ namespace RocketChat\Api;
  */
 class User extends AbstractApi
 {
+    private $userList = null;
 
     /**
      * Returns tokens for user
@@ -30,4 +31,57 @@ class User extends AbstractApi
 
         return null;
     }
+
+    public function lookup($username)
+    {
+        $this->populateUserList();
+
+        if (array_key_exists($username, $this->userList))
+        {
+            return $this->userList[$username];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private function populateUserList()
+    {
+        if (! is_null($this->userList))
+        {
+            return;
+        }
+
+        $result = (object)[
+            'users' => [],
+            'total' => 0,
+            'count' => 0,
+            'success' =>1,
+        ];
+        $offset=0;
+        do
+        {
+            $partial = $this->get("users.list?count=100&offset={$offset}");
+            if (!$this->status)
+            {
+                return null;
+            }
+            $result->total = (int)$partial->total;
+            $result->users = array_merge($result->users,$partial->users);
+            $offset+=(int)$partial->count;
+        }
+        while( count($result->users)< $result->total);
+
+        $this->userList = array();
+        foreach ($result->users as $user)
+        {
+            if (isset($user->username))
+            {
+                $this->userList[$user->username] = $user;
+            }
+        }
+    }
 }
+
+?>
